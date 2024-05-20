2cceb06b6d4c73d7471ea89aa2a033763bf2b770
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2024-05-20 09:46:22 +0600

    reorganized components

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import Banner from '@/app/(home)/components/Banner';
+import Image from 'next/image';
+
+export default function Home() {
+    return (
+        <>
+            <Banner />
+            
+        </>
+    );
+}
